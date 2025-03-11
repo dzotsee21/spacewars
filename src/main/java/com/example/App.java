b9106e4361f4448016bf0a6482708624fef13175
LukZe21 <ldzotsenidze4@gmail.com>
2025-03-11 17:04:35 +0400

    added shooting system and enemy object destroying system

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,12 @@
 package com.example;
 
 import javafx.animation.AnimationTimer;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -12,15 +14,19 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * JavaFX App
@@ -42,28 +48,31 @@ public class App extends Application {
         Controller controller = loader.getController();
         Scene scene = new Scene(root);
 
-        setupKeyHandlers(scene, controller);
-        mainGameLoop(controller);
+
+        Pane rootPane = (Pane) root;
+
+        setupKeyHandlers(scene, controller, rootPane);
+        mainGameLoop(controller, stage);
 
         stage.setScene(scene);
         stage.show();
     }
 
-    private void mainGameLoop(Controller controller) {
+    private void mainGameLoop(Controller controller, Stage stage) {
         AnimationTimer gameLoop = new AnimationTimer() {
             @Override
             public void handle(long now) {
                 // movements
-                if(pressedForward) {
-                    controller.moveUp();
+                if(pressedForward && (controller.playerY+333) > 0) {
+                        controller.moveUp();
                 }
-                if(pressedBackward) {
+                if(pressedBackward && (controller.playerY+333) < stage.getHeight()-40) {
                     controller.moveDown();
                 }
-                if(pressedRight) {
+                if(pressedRight && (controller.playerX+300) < stage.getWidth()-5) {
                     controller.moveRight();
                 }
-                if(pressedLeft) {
+                if(pressedLeft && (controller.playerX+300) > 0) {
                     controller.moveLeft();
                 }
             }
@@ -71,7 +80,7 @@ public class App extends Application {
         gameLoop.start();
     }
 
-    private void setupKeyHandlers(Scene scene, Controller controller) {
+    private void setupKeyHandlers(Scene scene, Controller controller, Pane rootPane) {
         scene.setOnKeyPressed(event -> {
             switch(event.getCode()) {
                 case W:
@@ -104,7 +113,25 @@ public class App extends Application {
                     pressedLeft = false;
                     break; 
                 case SPACE:
-                    controller.onPlayAudio();         
+                    controller.onPlayAudio();
+                    HashMap<Circle, List<Double>> enemyPositions = controller.getEnemyPos();
+                    Line ammo = controller.shootAmmo();
+
+                    rootPane.getChildren().add(ammo);
+
+                    TranslateTransition translate = new TranslateTransition();
+                    translate.setNode(ammo);
+                    translate.setByY(ammo.getStartY()-1500);
+                    translate.setDuration(Duration.millis(1000));
+                    translate.play();
+
+                    for(Circle object : enemyPositions.keySet()) {
+                        if(ammo.getStartX() >= enemyPositions.get(object).get(0) && ammo.getStartX() <= enemyPositions.get(object).get(1)) {
+                            rootPane.getChildren().remove(object);
+                        }
+                    }
+
+                    break;
             }
         });
     }
--- src/main/java/com/example/Controller.java
+++ src/main/java/com/example/Controller.java
@@ -1,24 +1,42 @@
 package com.example;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
-import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
 
 public class Controller {
-    double moveSpeed = 2;
+    private double moveSpeed = 4;
+
+    private double ammoStartX;
+    private double ammoStartY;
+    private double ammoEndX;
+    private double ammoEndY;
 
     @FXML
     private Circle player;
-    double x;
-    double y;
+    double playerX;
+    double playerY;
+    @FXML
+    private Circle enemyObject;
+    private double enemyObjectX;
+    private double enmyObjectY;
+
     @FXML
     private MediaView mediaView;
 
-    @FXML
+
+    // methods
     public void onPlayAudio() {
         try {
             String path = getClass().getResource("shoot.wav").toURI().toString();
@@ -32,19 +50,46 @@ public class Controller {
     }
 
     public void moveUp() {
-        player.setCenterY(y-=moveSpeed);
+        player.setCenterY(playerY-=moveSpeed);
     }
 
     public void moveDown() {
-        player.setCenterY(y+=moveSpeed);
+        player.setCenterY(playerY+=moveSpeed);
     }
 
     public void moveRight() {
-        player.setCenterX(x+=moveSpeed);
+        player.setCenterX(playerX+=moveSpeed);
     }
 
     public void moveLeft() {
-        player.setCenterX(x-=moveSpeed);
+        player.setCenterX(playerX-=moveSpeed);
     }
 
+    public Line shootAmmo() {
+        Line ammo = new Line();
+        ammo.setFill(Color.AQUA);
+        ammoStartX = playerX+300;
+        ammoStartY = playerY+313;
+
+        ammoEndX = playerX+300;
+        ammoEndY = playerY+333;
+        
+        ammo.setStartX(ammoStartX);
+        ammo.setStartY(ammoStartY);
+
+        ammo.setEndX(ammoEndX);
+        ammo.setEndY(ammoEndY);
+
+        return ammo;
+    }
+
+    public HashMap<Circle, List<Double>> getEnemyPos() {
+        double startX = ((enemyObject.getCenterX()+300)-enemyObject.getRadius());
+        double endX = ((enemyObject.getCenterX()+300)+enemyObject.getRadius());
+
+        HashMap<Circle, List<Double>> enemyPos = new HashMap<Circle, List<Double>>();
+        enemyPos.put(enemyObject, Arrays.asList(startX, endX));
+
+        return enemyPos;
+    }
 }
